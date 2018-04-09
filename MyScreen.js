11dc078925ee5f5d6a07399ff94171980592ffe3
wapchief <wapchief@qq.com>
2018-04-09 09:00:54 +0800

    TabView

---
+++ MyScreen.js
@@ -0,0 +1,10 @@
+
+import React,{Component} from 'react';
+import {View,Text} from 'react-native';
+export default class MyScreen extends Component {
+  render() {
+    return (
+      <Text>MyScreen</Text>
+    )
+  }
+}
---
+++ NewsScreen.js
@@ -0,0 +1,10 @@
+
+import React,{Component} from 'react';
+import {View,Text} from 'react-native';
+export default class NewsScreen extends Component {
+  render() {
+    return (
+      <Text>NewsScreen</Text>
+    )
+  }
+}
