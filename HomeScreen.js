481e03d25d34f4257bf1ed4d9be88fe79ddcc74b
wapchief <wapchief@qq.com>
2018-04-19 16:21:55 +0800

    load more

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,29 +1,53 @@
 /* 首页 */
 import React,{Component} from 'react';
-import { View, Text, Button, Image, StyleSheet, TouchableOpacity, ScrollView } from 'react-native'
+import {
+  View,
+  Text,
+  Button,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+  FlatList,
+  RefreshControl, ActivityIndicator
+} from 'react-native'
 import Swiper from 'react-native-swiper';
 import Dimensions from 'Dimensions'
 import Toast from 'react-native-root-toast'
 const screenWidth = Dimensions.get('window').width;
 const urlBanner = 'http://www.wanandroid.com/banner/json'
+let pageNo = 0;//当前第几页
+let itemNo = 0;
 export default class HomeScreen extends Component {
 
   constructor(props){
     super(props);
     this.state = {
       isShow:false,
+      //下拉刷新
+      refreshing: false,
+      //加载更多
+      isLoading: true,
+      dataArray: [],
+      // 控制foot， 0：隐藏footer  1：已加载完成,没有更多数据   2 ：显示加载中
+      showFoot: '1',
       data:'',
       items:[],
-      urls:[]
+      urls:[],
+      //列表数据
+      dataList:'',
+      //总页数
+      pageCount:0
     }
   }
 
   componentDidMount() {
     setTimeout(()=>{
       this.setState({
-        isShow: true
+        isShow: true,
       })
       this.getRequest(urlBanner)
+      this.getRequestList(0)
     },0)
   }
 
@@ -74,6 +98,30 @@ export default class HomeScreen extends Component {
     })
   }
 
+  /*轮播*/
+  _headerBannerView(){
+    if(this.state.isShow){
+      return (
+        <ScrollView style={{}}>
+          <Swiper
+            removeClippedSubviews={false}
+            key={this.state.urls.length}
+            autoplay = {true}
+            height = {200}
+            showsPagination = {true}
+            horizontal={true}>
+            {this._imageBanner()}
+          </Swiper>
+
+        </ScrollView>
+      )}else {
+      return (
+        <View style={styles.imgView}>
+          <Image source={ require('./res/drawable/head_other.png')} style={styles.bannerImg} />
+        </View>
+      )
+    }
+  }
 
   //请求banner
   getRequest (url) {
@@ -88,7 +136,7 @@ export default class HomeScreen extends Component {
       })
       .then((responseJson) => {
         this.setState({
-          data:responseJson.data,
+          data:responseJson.datas,
         });
         // alert(this.state.data[0].imagePath)
         this.addMap(responseJson.data)
@@ -98,41 +146,171 @@ export default class HomeScreen extends Component {
       })
   }
 
-  render() {
-    if(this.state.isShow){
-      return (
-      <ScrollView style={{}}>
-        <Swiper
-                removeClippedSubviews={false}
-                key={this.state.urls.length}
-                autoplay = {true}
-                height = {200}
-                showsPagination = {true}
-                horizontal={true}>
-          {this._imageBanner()}
-        </Swiper>
-        <Text
-          title="Go to My"
-          onPress={() => this.props.navigation.navigate('My')}
-        />
-        <Button
-          title="Go to News"
-          style={{margin:20,padding:20}}
-          onPress={() => this.props.navigation.navigate('News')}
-        />
-        <Button
-          title="Go to App"
-          style={{marginTop:10}}
-          onPress={() => Toast.show(this.state.data[0].title)}
-        />
-      </ScrollView>
-    )}else {
-        return (
-          <View style={styles.imgView}>
-            <Image source={ require('./res/drawable/head_other.png')} style={styles.bannerImg} />
-          </View>
-        )
+
+  /*加载列表数据*/
+  getRequestList(pagerNo){
+    const opts = {
+      method: 'GET',
     }
+    fetch('http://www.wanandroid.com/article/list/'+pagerNo+'/json', opts)
+      .then((response) => {
+        this.setState({refreshing: false});
+        return response.json();
+      })
+      .then((responseJson) => {
+        let data = responseJson.data.datas;
+        let dataBlob = [];
+        let i = itemNo;
+
+        data.map(function (item) {
+          dataBlob.push({
+            key: i,
+            value: item,
+          })
+          i++;
+        });
+        let foot = 0;
+        if(pageNo>=responseJson.data.pageCount){
+          //listView底部显示没有更多数据了
+          foot = 1;
+        }
+
+        this.setState({
+          dataList: responseJson.data.datas,
+          dataArray:this.state.dataArray.concat(dataBlob),
+          refreshing: false,
+          isLoading:false,
+          showFoot:foot,
+          pageCount:responseJson.data.pageCount,
+        });
+        //必须置空否则重复添加
+        data = null;
+        dataBlob = null;
+        // alert(this.state.dataList[0].title)
+      })
+      .catch((error) => {
+        alert(error)
+      })
+  }
+
+  //下拉刷新
+  _onRefresh () {
+    this.setState({refreshing: true})
+    this.getRequestList(0)
+  }
+
+  //上拉加载更多
+  _renderFooter(){
+    if (this.state.showFoot === 1) {
+      return (
+        <View style={{height:30,alignItems:'center',justifyContent:'flex-start',}}>
+          <Text style={{color:'#999999',fontSize:14,marginTop:5,marginBottom:5,}}>
+            没有更多数据了
+          </Text>
+        </View>
+      );
+    } else if(this.state.showFoot === 2) {
+      return (
+        <View style={styles.footer}>
+          <ActivityIndicator />
+          <Text>正在加载数据...</Text>
+        </View>
+      );
+    } else if(this.state.showFoot === 0){
+      return (
+        <View style={styles.footer}>
+        </View>
+      );
+    }else {
+      return(
+        <Text/>
+      )
+    }
+  }
+
+  _onEndReached(){
+    //如果是正在加载中或没有更多数据了，则返回
+    if(this.state.showFoot !== 0 ){
+      return ;
+    }
+    //如果当前页大于或等于总页数，那就是到最后一页了，返回
+    if((pageNo!==1) && (pageNo>=this.state.pageCount)){
+      return;
+    } else {
+      pageNo++;
+    }
+    //底部显示正在加载更多数据
+    this.setState({showFoot:2});
+    //获取数据
+    this.getRequestList(pageNo);
+  }
+
+
+  //列表点击事件
+  itemClick (item, index) {
+    // alert('新闻标题：' + item.author_name + '\n时间：' + item.date+'\n'+item.thumbnail_pic_s);
+    this.props.navigation.navigate('Details', {
+      title: item.title,
+      url: item.link,
+    })
+  }
+  //列表分割线
+  _itemDivide = () => {
+    return (
+      <View style={{height: 10}}/>
+    )
+  }
+  //FlatList的key
+  _keyExtractor = (item, index) => index.toString()
+
+  //子item渲染
+  _renderItem = ({item, index}) => {
+    return (
+      <TouchableOpacity
+        activeOpacity={0.5}
+        onPress={this.itemClick.bind(this, item, index)}>
+        <View style={{backgroundColor: '#ffffff', padding: 10, flexDirection: 'row'}}>
+
+          <View style={{flex: 1, flexDirection: 'column'}}>
+
+            <Text style={{
+              marginRight: 10,
+              marginLeft: 10,
+              width: screenWidth-20,
+              height: 80 * 0.7
+            }}>{item.value.title}</Text>
+
+            <View style={{flexDirection: 'row', paddingLeft: 10, paddingRight: 10}}>
+              <Text style={styles.subTitle}>{item.value.chapterName}</Text>
+              <Text style={styles.subTitle}> {item.value.niceDate}</Text>
+            </View>
+          </View>
+
+        </View>
+
+      </TouchableOpacity>
+    )
+  }
+
+  render() {
+    return(
+      <FlatList
+        data={this.state.dataArray}
+        keyExtractor={this._keyExtractor}
+        renderItem={this._renderItem}
+        ListHeaderComponent={this._headerBannerView()}
+        ItemSeparatorComponent={this._itemDivide}
+        ListFooterComponent={this._renderFooter.bind(this)}
+        onEndReached={this._onEndReached.bind(this)}
+        onEndReachedThreshold={1}
+        refreshControl={
+          <RefreshControl
+            refreshing={this.state.refreshing}
+            onRefresh={this._onRefresh.bind(this)}
+          />
+        }
+      />
+    )
   }
 }
 
@@ -156,5 +334,17 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 200,
     flex: 1
-  }
+  },
+  imgStyles: {
+    width: screenWidth * 0.3,
+    height: 80
+  },
+  footer:{
+    flexDirection:'row',
+    height:24,
+    justifyContent:'center',
+    alignItems:'center',
+    marginBottom:10,
+  },
+
 })
--- NewsScreen.js
+++ NewsScreen.js
@@ -10,66 +10,74 @@ import {
   RefreshControl
 } from 'react-native'
 import Dimensions from 'Dimensions'
-import ScrollableTabView, {DefaultTabBar, ScrollableTabBar} from 'react-native-scrollable-tab-view';
+import ScrollableTabView, { DefaultTabBar, ScrollableTabBar } from 'react-native-scrollable-tab-view'
 import Toast from 'react-native-root-toast'
 
 const newsUrl = 'http://toutiao-ali.juheapi.com/toutiao/index'
 const appCode = 'APPCODE b8a415fd4c884651ba74827cdbe3ccbc'
 const header = new Headers()
-const screenWidth = Dimensions.get('window').width;
+const screenWidth = Dimensions.get('window').width
 // 类型,,top(头条，默认),shehui(社会),guonei(国内),guoji(国际),yule(娱乐),tiyu(体育)junshi(军事),keji(科技),caijing(财经),shishang(时尚)
-const news = ['top','shehui','guonei','guoji','yule','tiyu','junshi','keji','caijing','shishang']
+const news = ['top', 'shehui', 'guonei', 'guoji', 'yule', 'tiyu', 'junshi', 'keji', 'caijing', 'shishang']
 export default class NewsScreen extends Component {
 
-  constructor(props){
-    super(props);
+  constructor (props) {
+    super(props)
     //在这里定义json返回的key
-    this.state={
+    this.state = {
+      //控制分类展示
+      isShow: false,
       //分类
-      newsType:'top',
+      newsType: 'top',
       //下拉刷新
       refreshing: false,
       //data数据
-      resultJson:null,
-      error_code:'',
-      reason:'',
+      resultJson: null,
+      error_code: '',
+      reason: '',
       result: {
         data: ''
       }
-    };
+    }
   }
 
   //在最初的render方法调用之后立即调用。
   //网络请求、事件订阅等操作可以在这个方法中调用。
   //作用相同与Fragment生命周期中的onViewCreate方法。
-  componentDidMount(){
-    this.getRequest(newsUrl,this.state.newsType);
+  componentDidMount () {
+    // Workaround for #470: github.com/skv-headless/react-native-scrollable-tab-view/issues/470
+    // setTimeout(() => {
+    //   this.setState({
+    //     isShow:true
+    //   })
+    // }, 1);
+    this.getRequest(newsUrl, this.state.newsType)
   }
 
-  getRequest (url,type) {
+  getRequest (url, type) {
     /*网络请求的配置*/
     header.append('Authorization', appCode)
     const opts = {
       method: 'GET',
       headers: header,
     }
-    fetch(url+'?type='+type, opts)
+    fetch(url + '?type=' + type, opts)
       .then((response) => {
-        this.setState({refreshing: false});
-        return response.json();
+        this.setState({refreshing: false})
+        return response.json()
       })
       .then((responseJson) => {
         this.setState({
-          resultJson:responseJson,
-          error_code:responseJson.error_code,
-          reason:responseJson.reason,
-          result:responseJson.result,
-          data:responseJson.result.data,
+          resultJson: responseJson,
+          error_code: responseJson.error_code,
+          reason: responseJson.reason,
+          result: responseJson.result,
+          data: responseJson.result.data,
           // name:responseJson.result.data.name,
           // date:responseJson.result.data.date,
           // title:responseJson.result.data.title,
 
-        });
+        })
         // alert(this.state.reason)
       })
       .catch((error) => {
@@ -78,22 +86,22 @@ export default class NewsScreen extends Component {
   }
 
   //下拉刷新
-  _onRefresh(type) {
-    this.setState({refreshing: true});
-    this.getRequest(newsUrl,type)
+  _onRefresh (type) {
+    this.setState({refreshing: true})
+    this.getRequest(newsUrl, type)
   }
 
   //列表点击事件
-  itemClick(item, index) {
+  itemClick (item, index) {
     // alert('新闻标题：' + item.author_name + '\n时间：' + item.date+'\n'+item.thumbnail_pic_s);
     this.props.navigation.navigate('Details', {
       title: item.title,
-      url:item.url,
+      url: item.url,
     })
   }
 
   //FlatList的key
-  _keyExtractor = (item, index) => index.toString();
+  _keyExtractor = (item, index) => index.toString()
 
   //子item渲染
   _renderItem = ({item, index}) => {
@@ -101,16 +109,21 @@ export default class NewsScreen extends Component {
       <TouchableOpacity
         activeOpacity={0.5}
         onPress={this.itemClick.bind(this, item, index)}>
-        <View style={{backgroundColor:'#ffffff',padding:10,flexDirection:'row'}}>
+        <View style={{backgroundColor: '#ffffff', padding: 10, flexDirection: 'row'}}>
 
-          <Image source={{uri:item.thumbnail_pic_s02}}  style={styles.imgStyle}/>
+          <Image source={{uri: item.thumbnail_pic_s02}} style={styles.imgStyle}/>
 
-          <View style={{flex:1,flexDirection:'column'}}>
+          <View style={{flex: 1, flexDirection: 'column'}}>
 
-          <Text style={{paddingRight:10,marginLeft:10,width:screenWidth*0.65,height:80*0.7}}>{item.title}</Text>
+            <Text style={{
+              paddingRight: 10,
+              marginLeft: 10,
+              width: screenWidth * 0.65,
+              height: 80 * 0.7
+            }}>{item.title}</Text>
 
-            <View style={{flexDirection:'row',alignItems:'center',paddingLeft:10,paddingRight:10}}>
-              <Text style={styles.subTitle}>{item.category+'  '+item.author_name}</Text>
+            <View style={{flexDirection: 'row', paddingLeft: 10, paddingRight: 10}}>
+              <Text style={styles.subTitle}>{item.category + '  ' + item.author_name}</Text>
               <Text style={styles.subTitle}> {item.date}</Text>
             </View>
           </View>
@@ -118,27 +131,28 @@ export default class NewsScreen extends Component {
         </View>
 
       </TouchableOpacity>
-    );
+    )
   }
   //headerList
   /*ListHeaderComponent={this._headerView()}*/
-  _headerView(){
+  _headerView () {
     return (
       <View>
-      <Text>{'reason:'+this.state.reason+'\ncode:'+this.state.error_code}</Text>
-      <Text style={styles.text} onPress={() => {
-      /* 1. Navigate to the Details route with params */
-      this.props.navigation.navigate('Details', {
-        itemId: 86,
-        otherParam: 'First Details',
-      })
-    }}>新闻详情</Text>
-    <Button title={'刷新列表'} style={styles.text} onPress={() => this.getRequest(newsUrl,this.state.newsType)}/>
+        <Text>{'reason:' + this.state.reason + '\ncode:' + this.state.error_code}</Text>
+        <Text style={styles.text} onPress={() => {
+          /* 1. Navigate to the Details route with params */
+          this.props.navigation.navigate('Details', {
+            itemId: 86,
+            otherParam: 'First Details',
+          })
+        }}>新闻详情</Text>
+        <Button title={'刷新列表'} style={styles.text} onPress={() => this.getRequest(newsUrl, this.state.newsType)}/>
       </View>
-  )
+    )
   }
+
   /*tabType*/
-  _headerTabView(){
+  _headerTabView () {
     return (
       <ScrollableTabView
         initialPage={0}
@@ -167,26 +181,25 @@ export default class NewsScreen extends Component {
   _itemDivide = () => {
     return (
       <View style={{height: 10}}/>
-    );
+    )
   }
-
 
   render () {
 
     return (
-        <FlatList
-          data={this.state.result.data}
-          keyExtractor={this._keyExtractor}
-          renderItem={this._renderItem}
-          ListHeaderComponent={this._headerTabView()}
-          ItemSeparatorComponent={this._itemDivide}
-          refreshControl={
-            <RefreshControl
-              refreshing={this.state.refreshing}
-              onRefresh={this._onRefresh.bind(this,this.state.newsType)}
-            />
-          }
-        />
+      <FlatList
+        data={this.state.result.data}
+        keyExtractor={this._keyExtractor}
+        renderItem={this._renderItem}
+        ListHeaderComponent={this._headerTabView()}
+        ItemSeparatorComponent={this._itemDivide}
+        refreshControl={
+          <RefreshControl
+            refreshing={this.state.refreshing}
+            onRefresh={this._onRefresh.bind(this, this.state.newsType)}
+          />
+        }
+      />
 
     )
 
@@ -197,14 +210,14 @@ const styles = StyleSheet.create({
   text: {
     marginTop: 20
   },
-  subTitle:{
-   marginTop:10,
-   color:'#666666',
-    fontSize:12,
-    flex:1
+  subTitle: {
+    marginTop: 10,
+    color: '#666666',
+    fontSize: 12,
+    flex: 1
   },
-  imgStyle:{
-    width:screenWidth*0.3,
-    height:80
+  imgStyle: {
+    width: screenWidth * 0.3,
+    height: 80
   }
 })
